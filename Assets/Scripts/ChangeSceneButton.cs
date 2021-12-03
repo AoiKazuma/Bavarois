6a6122513bee7e9416e85c42c6f7e54b39391a3d
iribe-k <kentaro.iribe@gmail.com>
2021-12-03 12:56:11 +0900

    データのセーブ機能実装

---
+++ Assets/Scripts/ChangeSceneButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChangeSceneButton : MonoBehaviour
+{
+
+    public void OnSelect()
+    {
+        //この中にボタンが押されたときの処理を書く。
+        //今回はシーンを変更させる処理を書く。
+        //ちなみにこの関数の名前はOnSelectである必要はないです。
+    }
+}
