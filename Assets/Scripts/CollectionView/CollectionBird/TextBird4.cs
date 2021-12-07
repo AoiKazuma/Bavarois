984a67d048bb5282077a9c71f5d836eaa9ee64fd
青井一真 <>
2021-12-07 11:33:20 +0900

    コレクション画面の型を完成させました。

---
+++ Assets/Scripts/CollectionView/CollectionBird/TextBird4.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class TextBird4 : MonoBehaviour
+{
+    public Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        SaveData saveData = DataManager.instance.Load();
+        
+        if(saveData.collection_bird[4]==0){
+            text.enabled = false;
+        }
+        else{
+            text.enabled = true;
+        }
+    }
+
+}
