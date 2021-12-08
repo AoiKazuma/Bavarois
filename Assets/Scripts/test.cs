9305dc076196c41fc725c783b73616fd8513e524
小橋駿平 <>
2021-12-08 13:26:59 +0900

    no message

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -9,6 +9,8 @@ public class test : MonoBehaviour
     [SerializeField]
     PieChart _pie;
     [SerializeField] private SaveData saveData;
+
+    [SerializeField] private GameObject gameObject_1;
     
     private Text shoppingPer;
     private Text mealPer;
@@ -16,10 +18,14 @@ public class test : MonoBehaviour
     private Text fixedPer;
     private Text otherPer;
     private int sum=0;
+    // public int target_yyyy;
+    public int gameObject_1_yyyy;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameObject_1_yyyy = gameObject_1.GetComponent<GraphScript>().target_yyyy;
+
         SaveData saveData = DataManager.instance.Load();
         float[] category_sums = new float[]{0,0,0,0,0};
 
@@ -34,12 +40,14 @@ public class test : MonoBehaviour
         GameObject OtherPer = GameObject.Find("OtherPer");
         otherPer = OtherPer.GetComponent<Text>();
 
-
-        for  (int r=0; r<saveData.log_list.Count; r++){
-            // Debug.Log(saveData.log_list[r].type);
-            category_sums[saveData.log_list[r].type] += saveData.log_list[r].amount;
-            sum += saveData.log_list[r].amount;
+        Debug.Log(gameObject_1_yyyy);
+        for (int r=0; r<saveData.log_list.Count; r++){
+            if (saveData.log_list[r].yyyy==gameObject_1_yyyy){
+                category_sums[saveData.log_list[r].type] += saveData.log_list[r].amount;
+                sum += saveData.log_list[r].amount;
+            }
         }
+
         shoppingPer.text = Math.Round(((double)category_sums[0]/(double)sum)*100) + "%";
         mealPer.text = Math.Round(((double)category_sums[1]/(double)sum)*100) + "%" ;
         trafficPer.text = Math.Round(((double)category_sums[2]/(double)sum)*100) + "%" ;
@@ -52,6 +60,38 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void Retest(){
+        gameObject_1_yyyy = gameObject_1.GetComponent<GraphScript>().target_yyyy;
+        SaveData saveData = DataManager.instance.Load();
+        float[] category_sums = new float[]{0,0,0,0,0};
+
+        GameObject ShoppingPer = GameObject.Find("ShoppingPer");
+        shoppingPer = ShoppingPer.GetComponent<Text>();
+        GameObject MealPer = GameObject.Find("MealPer");
+        mealPer = MealPer.GetComponent<Text>();
+        GameObject TrafficPer = GameObject.Find("TrafficPer");
+        trafficPer = TrafficPer.GetComponent<Text>();
+        GameObject FixedPer = GameObject.Find("FixedPer");
+        fixedPer = FixedPer.GetComponent<Text>();
+        GameObject OtherPer = GameObject.Find("OtherPer");
+        otherPer = OtherPer.GetComponent<Text>();
+
+        Debug.Log(gameObject_1_yyyy);
+        for (int r=0; r<saveData.log_list.Count; r++){
+            if (saveData.log_list[r].yyyy==gameObject_1_yyyy){
+                category_sums[saveData.log_list[r].type] += saveData.log_list[r].amount;
+                sum += saveData.log_list[r].amount;
+            }
+        }
+
+        shoppingPer.text = Math.Round(((double)category_sums[0]/(double)sum)*100) + "%";
+        mealPer.text = Math.Round(((double)category_sums[1]/(double)sum)*100) + "%" ;
+        trafficPer.text = Math.Round(((double)category_sums[2]/(double)sum)*100) + "%" ;
+        fixedPer.text = Math.Round(((double)category_sums[3]/(double)sum)*100) + "%" ;
+        otherPer.text = Math.Round(((double)category_sums[4]/(double)sum)*100) + "%"  ;
+
+        _pie.SetPieChartAnimation(category_sums);
     }
 }
